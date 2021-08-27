72a5b93761d4a47eac3ac9221a48d77f57962a58
Thaynara Araújo <thaynara_araujo1@yahoo.com.br>
2021-08-26 21:00:56 -0300

    corrigindo erro de porta e cors

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ const swaggerUI = require('swagger-ui-express');
 const swaggerDocs = require('./swagger.json');
 const routes = require('./routes');
 const app = express();
+const cors = require('cors');
 
 mongoose.connect('mongodb+srv://bancoCurriculos:SxrqJBiPtKPI2pyI@cluster0.wejn0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
 	userNewUrlParser: true,
 	useCreateInder: true
 }); 
 
+app.use('cors'());
 app.use(express.json());
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use(routes);
+app.listen('3000', () => {
+	console.log('rodando na porta 3000');
+})
