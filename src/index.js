e01e30c5406efc6e775158257446f2ebcc84a804
Thaynara Araújo <thaynara_araujo1@yahoo.com.br>
2021-08-28 20:32:11 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -9,10 +9,16 @@ const cors = require('cors');
 mongoose.connect('mongodb+srv://bancoCurriculos:gGRjTqSp68KZ5ITa@gama.wejn0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
 }); 
 
+
+
 app.use(cors());
 app.use(express.json());
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use(routes);
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+});
+app.listen(process.env.PORT || 3000);
 app.listen('3000', () => {
 	console.log('rodando na porta 3000');
 })
